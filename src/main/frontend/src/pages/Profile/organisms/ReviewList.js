642c5778d432607a21788892d211077f0998cef9
ParkYongHo1 <qkaxhf8823@naver.com>
2024-08-09 15:54:20 +0900

    💪 프로필 탭 페이지 생성

---
+++ src/main/frontend/src/pages/Profile/organisms/ReviewList.js
@@ -0,0 +1,13 @@
+import Container from "../atom/Container";
+import styled from "@emotion/styled";
+const ReviewList = () => {
+  return (
+    <>
+      <Container></Container>
+    </>
+  );
+};
+const Container = styled.div`
+  width: 100%;
+`;
+export default ReviewList;
---
+++ src/main/frontend/src/pages/Profile/organisms/WriteList.js
@@ -0,0 +1,13 @@
+import Container from "../atom/Container";
+import styled from "@emotion/styled";
+const WriteList = () => {
+  return (
+    <>
+      <Container></Container>
+    </>
+  );
+};
+const Container = styled.div`
+  width: 100%;
+`;
+export default WriteList;
